7fe75f9a1f93201c04e18a2cf8c95541628906ee
Avrumi Friedman <a.friedman07@gmail.com>
2024-06-19 19:59:59 -0400

    Added layout awesomeness

--- ReactComponentsDemo.Web/ClientApp/src/components/Layout.jsx
+++ ReactComponentsDemo.Web/ClientApp/src/components/Layout.jsx
@@ -7,7 +7,7 @@ const Layout = ({ children }) => {
             <header>
                 <nav className="navbar navbar-expand-sm navbar-dark fixed-top bg-dark border-bottom box-shadow">
                     <div className="container">
-                        <a className="navbar-brand">ReactComponentsDemo</a>
+                        <a className="navbar-brand">ReactComponentsDemo!!</a>
                         <button className="navbar-toggler" type="button" data-toggle="collapse"
                             data-target=".navbar-collapse" aria-controls="navbarSupportedContent"
                             aria-expanded="false" aria-label="Toggle navigation">
